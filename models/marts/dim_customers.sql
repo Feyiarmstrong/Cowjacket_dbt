ad8ea4a55431cadfb4c8d9c96b38e18a94cf684e
Feyisayo Ajiboye <solapeajiboye@gmail.com>
2025-12-14 03:03:48 +0100

    Update dim_customers.sql

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,3 +1,13 @@
+{{ config(
+    materialized='table',
+    schema='analytics_marts' if target.name == 'production' else target.schema
+) }}
+
+{% if target.name != 'production' and config.get('schema') == 'analytics_marts' %}
+  {{ exceptions.raise_compiler_error("analytics_marts is PROD-ONLY") }}
+{% endif %}
+
+
 select
     c.customer_id,
     c.full_name,
@@ -7,4 +17,5 @@ select
     l.last_transaction_date
 from {{ ref('stg_customers') }} c
 left join {{ ref('int_loyalty_summary') }} l
+
     on c.customer_id = l.customer_id
