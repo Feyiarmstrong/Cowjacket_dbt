b421e6452ac90b5a3bca5e4787b4fd72860e71bb
Feyisayo Ajiboye <solapeajiboye@gmail.com>
2026-01-04 16:42:29 +0100

    Initial commit

--- models/marts/fact_product_sales.sql
+++ models/marts/fact_product_sales.sql
@@ -1,10 +1,10 @@
 {{ config(
     materialized='table',
-    schema='dbt_production' if target.name == 'production' else target.schema
+    schema='analytics_marts' if target.name == 'production' else target.schema
 ) }}
 
-{% if target.name != 'production' and config.get('schema') == 'dbt_production' %}
-  {{ exceptions.raise_compiler_error("dbt_production is PROD-ONLY") }}
+{% if target.name != 'production' and config.get('schema') == 'analytics_marts' %}
+  {{ exceptions.raise_compiler_error("analytics_marts is PROD-ONLY") }}
 {% endif %}
 
 
@@ -23,4 +23,5 @@ group by
     p.product_name,
 
     p.category
+
 
