e032c6746fb2001dc65642618e7af205a0acb802
Feyisayo Ajiboye <solapeajiboye@gmail.com>
2026-01-04 17:03:45 +0100

    Update fact_orders.sql

--- models/marts/fact_orders.sql
+++ models/marts/fact_orders.sql
@@ -1,12 +1,3 @@
-{{ config(
-    materialized='table',
-    schema='analytics_marts' if target.name == 'production' else target.schema
-) }}
-
-{% if target.name != 'production' and config.get('schema') == 'analytics_marts' %}
-  {{ exceptions.raise_compiler_error("analytics_marts is PROD-ONLY") }}
-{% endif %}
-
 select
     order_id,
     customer_id,
@@ -18,4 +9,5 @@ select
     order_date
 
 from {{ ref('int_customer_orders') }}
+
 
